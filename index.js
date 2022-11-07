0ad3265da17784dd984904914da1afc92c4c8812
Tchinonso <lightersmercury@gmail.com>
2022-11-07 16:33:01 +0100

    error trigger and calculator done

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-var errorParagragh = document.getElementById("error")
+ var errorParagragh = document.getElementById("error")
 
-function purchase() {
-	errorParagragh.textContent = "Something went wrong, try again"
+ function purchase() {
+ 	errorParagragh.textContent = "Something went wrong, try again"
+ }
+
+let num1 = 8
+let num2 = 2
+
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
+const add = () => {
+	let result = num1 + num2
+	sumEl.textContent = "sum: " + result
+}
+const subtract = () => {
+	let result = num1 - num2;
+  sumEl.textContent = "sum: " + result;
+}
+const divide = () => {
+	let result = num1 / num2;
+  sumEl.textContent = "sum: " + result;
+}
+const multiply = () => {
+	let result = num1 * num2;
+  sumEl.textContent = "sum: " + result;
 }
